3e2e6bd22919e5c4119c898349156521df812293
akhand08 <adib08@outlook.com>
2024-03-24 16:02:36 -0300

    display_backward and append functionalities have been added to the doubly linked list

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -26,17 +26,29 @@ class DoublyLinkedList:
         if self.head == None:
             self.head = new_node
             self.tail = new_node
-        
+            return
+     
+            
         temp_node = self.tail
-        temp_node.next = new_node
         self.tail = new_node
+        temp_node.next = self.tail
+        self.tail.prev = temp_node
+        
+        
+        # print(temp_node.prev)
+        
+        # print(self.head == self.tail)
+        
+        # temp_node.next = new_node
+        # self.tail = new_node
+        # self.tail.prev = temp_node
         
     
     
     # print element from beginning to ending
     def display_forward(self):
         if self.head == None:
-            print("Empty Linked List")
+            print("Empty Doubly Linked List")
         
         curr_node = self.head
         print("Doubly Linked List from Beginning: ", end="")
@@ -45,10 +57,35 @@ class DoublyLinkedList:
             curr_node = curr_node.next
             if curr_node != None:
                 print(" -> ", end="")
+        print()
+                
+    
+    
+    def display_backward(self):
+        if self.tail == None:
+            print("Empty Double Linked List"); 
+            
+        curr_node = self.tail 
+        print("Doubly Linked List from Ending: ", end="")
+        while curr_node != None:
+            print(curr_node.value, end="")
+            curr_node = curr_node.prev
+            
+            if curr_node != None:
+                print(" -> ", end="")
+        print()
+        
 
 
- 
-doublyLinkedList = DoublyLinkedList()
-doublyLinkedList.append(10)
-doublyLinkedList.append(20)
-doublyLinkedList.display_forward()
+if __name__ == "__main__":
+    doublyLinkedList = DoublyLinkedList()
+    
+    doublyLinkedList.append(10)
+    doublyLinkedList.append(20)
+    doublyLinkedList.append(999)
+    doublyLinkedList.append(721)
+    
+    doublyLinkedList.display_forward()
+    doublyLinkedList.display_backward()
+    
+    
