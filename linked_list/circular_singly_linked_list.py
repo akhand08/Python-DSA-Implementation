00564dff52ad9bb651f701991661fc83288ca9d2
akhand08 <adib08@outlook.com>
2024-11-05 08:22:46 +0600

    Insertion on Circular Linked List based in given index function has been added

--- linked_list/circular_singly_linked_list.py
+++ linked_list/circular_singly_linked_list.py
@@ -10,7 +10,8 @@ class CircularSinglyLinkedList:
     def __init__(self):
         self.head = None
         self.length = 0
-        
+    
+    # add new element at the end of the list
     def append(self, value):
         new_node = Node(value)
         if self.head == None:
@@ -28,7 +29,7 @@ class CircularSinglyLinkedList:
         new_node.next = self.head
         self.length += 1
         
-    
+    # add new element at the beginning of the list
     def prepend(self, value):
         new_node = Node(value)
         if self.head == None:
@@ -43,6 +44,35 @@ class CircularSinglyLinkedList:
         temp_node.next = new_node
         new_node.next = self.head
         self.head = new_node
+
+    # 1-based indexing
+    def add_by_index(self, value, index):
+        if index < 1 or index > self.length + 1:
+            return False
+        
+        count_index = 0
+        curr_node = self.head
+        new_node = Node(value)
+
+        while count_index < index:
+            curr_node = curr_node.next
+            count_index += 1
+        
+        next_node = curr_node.next
+        curr_node.next = new_node
+        new_node.next = next_node
+
+
+
+
+if __name__ == '__main__':
+    circular_linked_list = CircularSinglyLinkedList()
+    
+
+
+        
+
+            
             
             
         
