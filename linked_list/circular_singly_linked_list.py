79a4d3aae38ed358c0ce8813c4a547f63f04170c
akhand08 <adib08@outlook.com>
2024-11-08 10:29:47 +0600

    Add display_circular_linked_list. Also fix the prepend method

--- linked_list/circular_singly_linked_list.py
+++ linked_list/circular_singly_linked_list.py
@@ -14,36 +14,51 @@ class CircularSinglyLinkedList:
     # add new element at the end of the list
     def append(self, value):
         new_node = Node(value)
+
         if self.head == None:
             self.head = new_node
+            new_node.next = self.head
             self.length += 1
             return
         
-        counter = 0
-        temp_node = self.head
+
+        counter = 1
+        curr_node = self.head
         while counter < self.length:
-            temp_node = temp_node.next
+            curr_node = curr_node.next
             counter += 1
         
-        temp_node.next = new_node
+
+        curr_node.next = new_node
         new_node.next = self.head
         self.length += 1
+
+
+
+
         
     # add new element at the beginning of the list
     def prepend(self, value):
         new_node = Node(value)
         if self.head == None:
             self.head = new_node
+            new_node.next = self.head
+            self.length += 1
+            return
         
-        counter = 0
-        temp_node = self.head
+        counter = 1
+        curr_node = self.head
         while counter < self.length:
-            temp_node = temp_node.next
+            curr_node = curr_node.next
             counter += 1
             
-        temp_node.next = new_node
+        curr_node.next = new_node
         new_node.next = self.head
         self.head = new_node
+        self.length += 1
+
+
+
 
     # 1-based indexing
     def add_by_index(self, value, index):
@@ -62,11 +77,37 @@ class CircularSinglyLinkedList:
         curr_node.next = new_node
         new_node.next = next_node
 
+    def display_circular_linked_list(self):
+        if self.head == None:
+            print("Empty Circular Linked List")
+            return
+        
+
+        curr_node = self.head
+        print("Circular Linked List --->  ", end = "")
+        while True:
+            print(curr_node.value, " --> ", end=" ")
+            curr_node = curr_node.next
+
+            if curr_node == self.head:
+                break
+        
+
+
 
 
 
 if __name__ == '__main__':
     circular_linked_list = CircularSinglyLinkedList()
+
+    circular_linked_list.append(10)
+    circular_linked_list.append(30)
+    circular_linked_list.append(40)
+    circular_linked_list.append(50)
+    circular_linked_list.prepend(100)
+    circular_linked_list.prepend(200)
+
+    circular_linked_list.display_circular_linked_list()
     
 
 
